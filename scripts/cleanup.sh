fd94f94f813d3f49f62923cd1b764511c8dcbaec
Pezhvak <pezhvakimv@gmail.com>
2022-04-23 03:30:34 +0400

    Added zip extension

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,5 +2,3 @@
 
 # Remove Build Dependencies
 apk del -f .build-deps
-rm /usr/local/bin/install-php-extensions
-rm /usr/local/bin/docker-php-*
--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env ash
 
 # Installing Composer
-install-php-extensions @composer-${COMPOSER_VERSION} imagick
+install-php-extensions @composer-${COMPOSER_VERSION} imagick zip
 # note: install-php-extensions installs are much easier than docker-php-ext-install
 
 # Installing PHP Extensions
 docker-php-ext-configure opcache --enable-opcache &&
   docker-php-ext-configure gd --enable-gd --with-freetype --with-jpeg --with-webp &&
-  docker-php-ext-configure zip &&
   docker-php-ext-install -j "$(nproc)" \
     mysqli \
     pdo_mysql \
@@ -18,6 +17,7 @@ docker-php-ext-configure opcache --enable-opcache &&
     bcmath \
     exif \
     imap
+
 
 # note: for some reason if we build gd with the rest of the extensions it will trow an error in php -v
 docker-php-ext-install -j "$(nproc)" gd
